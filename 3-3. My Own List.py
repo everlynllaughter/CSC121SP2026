6c5f7acf038cffb2f92e61fee1c964d54547d649
everlynllaughter <everlynllaughter@students.abtech.edu>
2026-02-11 00:05:11 -0500

    Add files via upload

---
+++ 3-3. My Own List.py
@@ -0,0 +1,7 @@
+
+transportation = [ "Mazda car", "plane", "mountain bike", "roller skates"]
+print (transportation)
+print (f"I love to drive my Mazda car.")
+print (f"I love to travel on a plane.")
+print (f"I enjoy riding a mountain bike in thew summer.")
+print (f"I am getting better at riding my roller skates.")
---
+++ 3-8. My Updated Guest List.py
@@ -0,0 +1,16 @@
+
+
+Guests = ["Irene Rex", "Lucy Rex", "Eunice Rex"]
+Guests.insert(0, "Michelle Obama")
+Guests.insert(2, "Clark Peoples")
+Guests.append( "Ayo Edebiri")
+Guests.sort()
+print("\nUpdated Guest List (Sorted):")
+print(Guests) 
+print("\nDinner Invitations :")
+print(f"Dear Michelle Obama, I would love to have you join me for dinner.")
+print(f"Dear Irene Rex, I would love to have you over for dinner.")
+print(f"Clark Peoples, I would love to have you join me for dinner.")
+print(f"Dear Lucy Rex, I would love to have you join me for dinner. ")
+print(f"Dear Eunice Rex, I would love to invite you over for dinner")
+print(f"Dear Ayo Edebiri, I would love to have you join me for dinner.")
