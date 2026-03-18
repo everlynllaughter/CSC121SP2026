e90c244c8ab06bc1e8bc9b506611b2164fbf55bc
everlynllaughter <everlynllaughter@students.abtech.edu>
2026-03-17 23:53:55 -0400

    Add files via upload

---
+++ 8.5.Cities.py
@@ -0,0 +1,9 @@
+def new_func():
+    def describe_city(city, country="USA"):
+        print(f"{city} is in {country}.")
+
+    describe_city("New York")
+    describe_city("Los Angeles")
+    describe_city("Calgary", "Canada")
+
+new_func()
